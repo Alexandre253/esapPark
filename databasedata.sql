fd7a3adb9cd5c2f284a3401689caa570705789aa
Alexandre253 <alexandrerede0@gmail.com>
2022-01-14 09:16:56 -0300

    remove trash

--- databasedata.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE ESAP
-USE ESAP
-CREATE TABLE ESAP(
-    placa varchar(10) primary key,
-    nome varchar(32) primary key,
-    horarioEntrada datetime null,
-    horarioSaida datetime null
-);
