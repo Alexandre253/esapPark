94fc768f3d2340d9fc4f76e536ba62a28addcab5
Alexandre253 <alexandrerede0@gmail.com>
2022-01-19 08:35:29 -0300

    att info

--- databasedata.sql
+++ databasedata.sql
@@ -6,3 +6,8 @@ CREATE TABLE ESAP(
     horarioEntrada datetime null,
     horarioSaida datetime null
 );
+insert into planilha("varData", "documento", "nome", "transportadora", "placa",
+					"horarioChegada", "horarioEntrada", "horarioSaida", 
+					 "cliente", "entregaColeta") 
+					 values (current_timestamp,'123456','Michael de Santa','Philips Enterprise',
+							 'EVK-2022',NULL, NULL, NULL, 'Gemla', 'Coleta'); 
