b1b66d4f0de7ce0753b51b8dcd0f2cc83e00c854
Alexandre253 <alexandrerede0@gmail.com>
2022-01-19 17:48:42 -0300

    reestruturacao getdados

--- funcoesBD.php
+++ funcoesBD.php
@@ -1,20 +1,31 @@
-<?
-include_once 'conectaBD';
-
+<?php
+include_once 'conectaBD.php';
+$host = 'localhost';
+$banco = 'esapbd';
+$usuario = 'postgres';
+$senha = 'alexandre11';
+$connString = "host=$host port=5432 dbname=$banco user=$usuario password=$senha";
+$conn = pg_connect($connString);
 //retorna as linhas da tabela
-function resultQuery(){
-$result = pg_query(resource $conn, string $query);
-for($i;$i<pg_num_rows($result;$i++){
- $array[$i] = pg_fetch_array($result, $i);
+//function resultQuery($query){
+function getDadosBD(){
+    $host = 'localhost';
+    $banco = 'esapbd';
+    $usuario = 'postgres';
+    $senha = 'alexandre11';
+    $connString = "host=$host port=5432 dbname=$banco user=$usuario password=$senha";
+    $conn = pg_connect($connString);
+    $row = 1;
+    $query = "SELECT * FROM planilha";
+    $result = pg_query($conn,$query);
+    //echo $rs['varData'];
+    //echo($rs["varData"]);
+    pg_close($conn);
+return $result;
 }
-echo $array['nome'];
-pg_close($conn);
-
-}
-
-//insere os dados na tabela
+//echo(getDadosBD());
 function inserirTabela($documento,$nome,$empresa,$cliente,$coletaEntrega){
-$query = "INSERT INTO planilha (data, documento, nome, transportadora, placa, horarioChegada, cliente, 
+$query = "INSERT INTO planilha (varData, documento, nome, transportadora, placa, horarioChegada, cliente, 
 coletaEntrega) values()";
 pg_query($query);
 pg_close($conn);
--- guarita.php
+++ guarita.php
@@ -1,5 +1,9 @@
 <?php
-include 'funcoes.php';
+include_once 'funcoes.php';
+include_once 'funcoesBD.php';
+include_once 'conectaBD.php';
+$connString = "host=$host port=5432 dbname=$banco user=$usuario password=$senha";
+$conn = pg_connect($connString);
 ?>
 <html>
 	<head>
@@ -12,7 +16,7 @@ include 'funcoes.php';
 	</p>
 
 	<?php
-		gerarTabelaGuarita(getArray());
+		gerarTabelaGuarita(getDadosBD());
 	?>
 	
 	</body>
